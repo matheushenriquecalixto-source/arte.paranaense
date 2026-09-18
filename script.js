ea6ec89535cc9e207738fb4152920276d4afc600
matheushenriquecalixto-source <matheus.henrique.calixto@escola.pr.gov.br>
2026-09-18 08:50:51 -0300

    atualizei o codigo

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const  botoesCurtir = document.querySelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventListener("click", curtir);
+    function curtir(){
+        const contador = botaoCurtir.queryselector("span");
+        if(curtiu == false){
+            contador.textContent++;
+            curtiu = true;}
+            else{
+                 contador.textContent--/;
+                 curtiu = false
+            }
+    }
+});
+    
 
